e2ada04d5b76a2701d0b41cff9bb28db7b61cbbb
dalalaljassem <dalal.aljassem@gmail.com>
2022-06-08 17:54:38 +0300

    Add and view categories backend works

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const connectDb = require("./database");
 const app = express();
+const CategoryRoutes = require("./api/category/cat.routes");
+const mongoose = require("mongoose");
 
 //////////////
 connectDb();
 
 app.use(express.json());
+app.use("/Category", CategoryRoutes);
 
-//middlewares
+//Not Found Middleware
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+  res.status(404).json({ message: "Path not found" });
 });
 
+//Error Handling Middleware
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    error: {
-      message: err.message || "Internal Server Error",
-    },
-  });
+  res
+    .status(err.status || 500)
+    .json({ message: "Internal Server Error" || err });
 });
+
+//PORT Number
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`The application is running on localhost:${PORT}`);
