bb36e58c5ecf5376e08856d4f167f5593d1604e4
danahAlajmi <danah38x@gmail.com>
2022-06-09 04:03:19 +0300

    Finish creat-recipe

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const connectDb = require("./database");
 const app = express();
+const cors = require("cors");
+const recipeRoutes = require("./api/recipe/recipe.routes");
 
 //////////////
 connectDb();
 
 app.use(express.json());
+app.use(cors());
+
+//routers
+app.use("/recipe", recipeRoutes);
 
 //middlewares
 app.use((req, res, next) => {
