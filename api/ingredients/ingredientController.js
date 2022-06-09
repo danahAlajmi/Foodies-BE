1fcdc08e546249b99e35f97b7ff3e46875513936
Good Lookin <r_a_msmlm@outlook.com>
2022-06-09 18:12:00 +0300

    DONE creating new ingredient

---
+++ api/ingredients/ingredientController.js
@@ -0,0 +1,31 @@
+const Ingredient = require("../../models/Ingredient");
+
+// exports.fetchIngredient = async (ingredientId, next) => {
+//   try {
+//     //get the ing id, then from tht id, get the corresponding recipe tht uses it and bring the recipe.
+//     const ingredient = await Ingredient.findById(ingredientId).populate(
+//       "Recipe"
+//     );
+//     return ingredient;
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+exports.ingredientCreate = async (req, res, next) => {
+  try {
+    const newIngredient = await Ingredient.create(req.body);
+    res.status(201).json(newIngredient);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// exports.ingredientGet = async (req, res) => {
+//   try {
+//     const ingredients = await Ingredient.find();
+//     res.json(ingredients);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const connectDb = require("./database");
 const app = express();
+const cors = require("cors");
+const ingredientRoutes = require("./api/ingredients/ingredientRoute");
 
 //////////////
 connectDb();
 
 app.use(express.json());
+
+//ROUTES
+app.use(cors());
+app.use("/ingredients", ingredientRoutes);
 
 //middlewares
 app.use((req, res, next) => {
